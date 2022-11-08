dc17624bac295b311cf1aff534e6b23ef2749154
Tony Wu <bobacrypto2@gmail.com>
2022-11-08 03:49:30 -0800

    Taking a different approach | Create just a factory function for todo tasks

--- src/script/todo_tasks.js
+++ src/script/todo_tasks.js
@@ -1,38 +1,9 @@
-const todo_task = () => {
+const todo_task = (title, desc, dueDate, priority, completion) => {
     return {
-        ...title(), 
-        ...description(),
-        ...dueDate(),
-        ...priority(),
-        ...completion(),
-    }
-}
-const title = () => {
-    return {
-        title: () => {}
-     }
-}
-
-const description = () => {
-    return {
-        description: () => {} // Code to retrieve description string
-    }
-}
-
-const dueDate = () => {
-    return {
-        dueDate: () => {} // Code to retrieve due date
-    }
-}
-
-const priority = () => {
-    return {
-        priority: () => {} // Code to set priority of task
-    }
-}
-
-const completion = () => {
-    return {
-        completion: () => {} // Code to determine if task has been completed
+        title,
+        desc,
+        dueDate,
+        priority,
+        completion,
     }
 }
