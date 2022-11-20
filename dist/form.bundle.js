d344291952f52847bf8350d1c12a50ae99871171
Tony Wu <bobacrypto2@gmail.com>
2022-11-20 04:32:03 -0800

    Add .svg icons to Project Cards

--- dist/form.bundle.js
+++ dist/form.bundle.js
@@ -26,7 +26,37 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mai
   \***********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Project\": () => (/* binding */ Project),\n/* harmony export */   \"projectDOM\": () => (/* binding */ projectDOM),\n/* harmony export */   \"projectList\": () => (/* binding */ projectList)\n/* harmony export */ });\nconst projectList = [];\nconst main = document.querySelector('main');\n\nconst Project = function(title, date, priority, desc) {\n    this.title = title;\n    this.date = date;\n    this.priority = priority;\n    this.desc = desc;\n}\n\nlet projectDOM = function() {\n    const projSections = ['complete', 'title', 'deadline', 'options'];\n    const projectContainer = document.createElement('div');\n    const projCard = document.createElement('div');\n    projSections.forEach(name => {\n        if (name == 'complete') {\n            const completeCheck = document.createElement('input');\n            completeCheck.setAttribute('class', `${name}`);\n            completeCheck.setAttribute('type', 'checkbox');\n            projCard.append(completeCheck);\n        } else {\n            const label = document.createElement('p');\n            \n            let capFirst = name.slice(0, 1);\n            let restOfName = name.slice(1);\n            label.innerHTML = `${capFirst.toUpperCase()}${restOfName}`;\n            label.setAttribute('class', `${name}`);\n            \n            projCard.setAttribute('id', `project-${projectList.length}`);\n            projCard.setAttribute('class', `project`);\n            \n            projectContainer.append(projCard)\n            projCard.append(label);\n        }\n    });\n\n\n    projectContainer.setAttribute('class', 'projContainer');\n    main.append(projectContainer);\n}\n\n//gh-pages\n\n//# sourceURL=webpack://todo-list/./src/script/mainContent.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Project\": () => (/* binding */ Project),\n/* harmony export */   \"projectDOM\": () => (/* binding */ projectDOM),\n/* harmony export */   \"projectList\": () => (/* binding */ projectList)\n/* harmony export */ });\n/* harmony import */ var _images_task_svgrepo_com_svg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../images/task-svgrepo-com.svg */ \"./src/images/task-svgrepo-com.svg\");\n/* harmony import */ var _images_trash_bin_svgrepo_com_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../images/trash-bin-svgrepo-com.svg */ \"./src/images/trash-bin-svgrepo-com.svg\");\n/* harmony import */ var _images_write_svgrepo_com_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../images/write-svgrepo-com.svg */ \"./src/images/write-svgrepo-com.svg\");\n\n\n\n\nconst projectList = [];\nconst main = document.querySelector('main');\n\nconst Project = function(title, date, priority, desc) {\n    this.title = title;\n    this.date = date;\n    this.priority = priority;\n    this.desc = desc;\n}\n\nlet projectDOM = function() {\n    const projSections = ['complete', 'title', 'deadline', 'options'];\n    const projectContainer = document.createElement('div');\n    const projCard = document.createElement('div');\n\n    projSections.forEach(name => {\n        switch(name) {\n            case 'complete':\n                const completeCheck = document.createElement('input');\n                completeCheck.setAttribute('class', `${name}`);\n                completeCheck.setAttribute('type', 'checkbox');\n                projCard.append(completeCheck);\n                break;\n            case 'options':\n                const optionIcons = document.createElement('div');\n\n                const taskIcon = new Image();\n                taskIcon.src = _images_task_svgrepo_com_svg__WEBPACK_IMPORTED_MODULE_0__;\n                const editIcon = new Image();\n                editIcon.src = _images_write_svgrepo_com_svg__WEBPACK_IMPORTED_MODULE_2__;\n                const trashIcon = new Image();\n                trashIcon.src = _images_trash_bin_svgrepo_com_svg__WEBPACK_IMPORTED_MODULE_1__;\n\n                optionIcons.setAttribute('class', `${name}`);\n                optionIcons.append(taskIcon, editIcon, trashIcon);\n                projCard.append(optionIcons);\n                break;\n            default:\n                const txtDetail = document.createElement('p');\n\n                let capFirst = name.slice(0, 1);\n                let restOfName = name.slice(1);\n                txtDetail.innerHTML = `${capFirst.toUpperCase()}${restOfName}`;\n\n                txtDetail.setAttribute('class', `${name}`);\n                projCard.setAttribute('id', `project-${projectList.length}`);\n                projCard.setAttribute('class', `project`);\n\n                projCard.append(txtDetail);\n                break;\n            }\n\n        projectContainer.append(projCard)\n    });\n    \n\n    projectContainer.setAttribute('class', 'projContainer');\n    main.append(projectContainer);\n}\n\n//gh-pages\n// if (name == 'complete') {\n//     const completeCheck = document.createElement('input');\n//     completeCheck.setAttribute('class', `${name}`);\n//     completeCheck.setAttribute('type', 'checkbox');\n//     projCard.append(completeCheck);\n// } else {\n//     const label = document.createElement('p');\n    \n//     let capFirst = name.slice(0, 1);\n//     let restOfName = name.slice(1);\n//     label.innerHTML = `${capFirst.toUpperCase()}${restOfName}`;\n//     label.setAttribute('class', `${name}`);\n    \n//     projCard.setAttribute('id', `project-${projectList.length}`);\n//     projCard.setAttribute('class', `project`);\n    \n//     projectContainer.append(projCard)\n//     projCard.append(label);\n// }\n\n//# sourceURL=webpack://todo-list/./src/script/mainContent.js?");
+
+/***/ }),
+
+/***/ "./src/images/task-svgrepo-com.svg":
+/*!*****************************************!*\
+  !*** ./src/images/task-svgrepo-com.svg ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"69a184040a0a2dc10a90.svg\";\n\n//# sourceURL=webpack://todo-list/./src/images/task-svgrepo-com.svg?");
+
+/***/ }),
+
+/***/ "./src/images/trash-bin-svgrepo-com.svg":
+/*!**********************************************!*\
+  !*** ./src/images/trash-bin-svgrepo-com.svg ***!
+  \**********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"caa05fbf0d96e546fcd2.svg\";\n\n//# sourceURL=webpack://todo-list/./src/images/trash-bin-svgrepo-com.svg?");
+
+/***/ }),
+
+/***/ "./src/images/write-svgrepo-com.svg":
+/*!******************************************!*\
+  !*** ./src/images/write-svgrepo-com.svg ***!
+  \******************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"43f44a67e1e048a1a460.svg\";\n\n//# sourceURL=webpack://todo-list/./src/images/write-svgrepo-com.svg?");
 
 /***/ })
 
@@ -69,6 +99,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -83,6 +125,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
--- src/script/mainContent.js
+++ src/script/mainContent.js
@@ -1,3 +1,7 @@
+import task from '../images/task-svgrepo-com.svg';
+import trash from '../images/trash-bin-svgrepo-com.svg';
+import edit from '../images/write-svgrepo-com.svg';
+
 export const projectList = [];
 const main = document.querySelector('main');
 
@@ -12,31 +16,69 @@ export let projectDOM = function() {
     const projSections = ['complete', 'title', 'deadline', 'options'];
     const projectContainer = document.createElement('div');
     const projCard = document.createElement('div');
-    projSections.forEach(name => {
-        if (name == 'complete') {
-            const completeCheck = document.createElement('input');
-            completeCheck.setAttribute('class', `${name}`);
-            completeCheck.setAttribute('type', 'checkbox');
-            projCard.append(completeCheck);
-        } else {
-            const label = document.createElement('p');
-            
-            let capFirst = name.slice(0, 1);
-            let restOfName = name.slice(1);
-            label.innerHTML = `${capFirst.toUpperCase()}${restOfName}`;
-            label.setAttribute('class', `${name}`);
-            
-            projCard.setAttribute('id', `project-${projectList.length}`);
-            projCard.setAttribute('class', `project`);
-            
-            projectContainer.append(projCard)
-            projCard.append(label);
-        }
-    });
 
+    projSections.forEach(name => {
+        switch(name) {
+            case 'complete':
+                const completeCheck = document.createElement('input');
+                completeCheck.setAttribute('class', `${name}`);
+                completeCheck.setAttribute('type', 'checkbox');
+                projCard.append(completeCheck);
+                break;
+            case 'options':
+                const optionIcons = document.createElement('div');
+
+                const taskIcon = new Image();
+                taskIcon.src = task;
+                const editIcon = new Image();
+                editIcon.src = edit;
+                const trashIcon = new Image();
+                trashIcon.src = trash;
+
+                optionIcons.setAttribute('class', `${name}`);
+                optionIcons.append(taskIcon, editIcon, trashIcon);
+                projCard.append(optionIcons);
+                break;
+            default:
+                const txtDetail = document.createElement('p');
+
+                let capFirst = name.slice(0, 1);
+                let restOfName = name.slice(1);
+                txtDetail.innerHTML = `${capFirst.toUpperCase()}${restOfName}`;
+
+                txtDetail.setAttribute('class', `${name}`);
+                projCard.setAttribute('id', `project-${projectList.length}`);
+                projCard.setAttribute('class', `project`);
+
+                projCard.append(txtDetail);
+                break;
+            }
+
+        projectContainer.append(projCard)
+    });
+    
 
     projectContainer.setAttribute('class', 'projContainer');
     main.append(projectContainer);
 }
 
 //gh-pages
+// if (name == 'complete') {
+//     const completeCheck = document.createElement('input');
+//     completeCheck.setAttribute('class', `${name}`);
+//     completeCheck.setAttribute('type', 'checkbox');
+//     projCard.append(completeCheck);
+// } else {
+//     const label = document.createElement('p');
+    
+//     let capFirst = name.slice(0, 1);
+//     let restOfName = name.slice(1);
+//     label.innerHTML = `${capFirst.toUpperCase()}${restOfName}`;
+//     label.setAttribute('class', `${name}`);
+    
+//     projCard.setAttribute('id', `project-${projectList.length}`);
+//     projCard.setAttribute('class', `project`);
+    
+//     projectContainer.append(projCard)
+//     projCard.append(label);
+// }
