d8d93fb3dd116b1c3c00840e57f38a9b735e7548
Tony Wu <bobacrypto2@gmail.com>
2022-12-10 00:35:15 -0800

    Add ability to edit and save tasks by clicking on the Title of task | Restyle task title text color and centered text

--- src/script/taskmodal.js
+++ src/script/taskmodal.js
@@ -19,7 +19,7 @@ export function taskModal() {
     const modalBackground = document.createElement('div');
     const modalForm = document.createElement('div');
     const formHeader = document.createElement('header');
-    const buttonClasses = ['new', 'edit'];
+    const buttonClasses = ['new', 'remove'];
 
     modalForm.appendChild(formHeader);
     for (let i = 0; i < buttonClasses.length; i++) {
@@ -42,41 +42,83 @@ export function newTaskEvent() {
     const newBtn = document.querySelector('.new');
     const modalForm = document.querySelector('.task-display');
     const taskDetails = document.querySelector('#task-details');
+    const taskDisplay = document.querySelector('#task-display');
+    const decisionBtn = document.querySelector('#decision');
     newBtn.addEventListener('click', () => {
+        if (taskDisplay.childNodes.length <= 5) {
         // Hide task modal
         modalForm.classList.add('hidden');
         modalForm.classList.remove('task-display');
         // Reveal task detail modal
         taskDetails.classList.toggle('hidden');
-        // taskDetails.classList.add('shown');
         taskDetails.classList.toggle('task-details');
+
+        // Display ' CREATE TASK ' button
+        decisionBtn.innerHTML = 'create task'
+        decisionBtn.classList.add('create-task');
+        decisionBtn.classList.remove('save-task');
+        }
     });
 }
 
-export function closeTaskDetails(exitBtn, modal) {
-    const taskDisplay = document.querySelector('#task-display');
-
-    exitBtn.addEventListener('click', () => {
-        // Hide task detail modal
-        modal.classList.toggle('task-details');
-        modal.classList.toggle('hidden');
+function toggleTaskDisplays(taskDisplay, detailsModal) {
+        detailsModal.classList.toggle('task-details');
+        detailsModal.classList.toggle('hidden');
         // Reveal overview of task modal
         taskDisplay.classList.toggle('hidden');
-        taskDisplay.classList.add('task-display');
+        taskDisplay.classList.toggle('task-display');
+}
+
+function taskEditDetails(task, taskDisplay, detailsModal, currTask) {
+    const titleSelect = document.querySelector('.title');
+    const descSelect = document.querySelector('.details');
+    const decisionBtn = document.querySelector('#decision');
+
+    
+    task.addEventListener('click', () => {
+        titleSelect.value = currTask.title;
+        descSelect.value = currTask.desc;
+        decisionBtn.innerHTML = 'save';
+        decisionBtn.classList.remove('create-task');
+        decisionBtn.classList.add('save-task');
+        toggleTaskDisplays(taskDisplay, detailsModal);
+    });
+
+    function saveTask() {
+        task.innerHTML = titleSelect.value;
+        currTask.title = titleSelect.value;
+        currTask.desc = descSelect.value;
+        titleSelect.value = '';
+        descSelect.value = '';
+        toggleTaskDisplays(taskDisplay, detailsModal);
+    }
+
+    taskEditDetails.saveTask = saveTask;
+}
+
+export function exitBtnEvent(exitBtn, detailsModal) {
+    const taskDisplay = document.querySelector('#task-display');
+    
+    exitBtn.addEventListener('click', () => {
+        // Resets title and description
+        const title = document.querySelector('.title');
+        const desc = document.querySelector('.details');
+    
+        title.value = '';
+        desc.value = '';
+        toggleTaskDisplays(taskDisplay, detailsModal);
     });
 }
 
-function createTaskDetails(createBtn) {
+function decisionBtnEvent(decisionBtn, detailsModal) {
     const title = document.querySelector('.title');
     const desc = document.querySelector('.details');
     const taskDisplay = document.getElementById('task-display');
-    createBtn.addEventListener('click', () => {
-        if (taskDisplay.childNodes.length <= 5 && title.value.length >= 3) {
-            taskList.push(new MakeTask(title.value, desc.value, taskList.length));
-            console.log(taskList);
-            const taskDetails = document.createElement('p');
-            taskDetails.innerHTML = taskList[taskList.length - 1].title;
-            taskDisplay.append(taskDetails);
+    decisionBtn.addEventListener('click', () => {
+        if (decisionBtn.innerHTML == 'create task' && taskDisplay.childNodes.length <= 5 && title.value.length >= 3) {
+            createTask(title, desc, taskDisplay, detailsModal);
+        } else if (decisionBtn.innerHTML == 'save' && taskDisplay.childNodes.length <= 5 && title.value.length >= 3) {
+            taskEditDetails.saveTask();
         } else {
             alert('Title must be 3 characters min.')
         }
@@ -84,10 +126,23 @@ function createTaskDetails(createBtn) {
 
 }
 
+function createTask(title, desc, taskDisplay, detailsModal) {
+        taskList.push(new MakeTask(title.value, desc.value, taskList.length));
+        let currTask = taskList[taskList.length - 1];
+        const taskDetails = document.createElement('p');
+        taskDetails.setAttribute('id', `task-${taskList.length - 1}`);
+        taskDetails.innerHTML = currTask.title;
+        title.value = '';
+        desc.value = '';
+        taskDisplay.append(taskDetails);
+        toggleTaskDisplays(taskDisplay, detailsModal);
+        taskEditDetails(taskDetails, taskDisplay, detailsModal, currTask);
+}
+
 export function newTaskDetails() {
     const modalSelect = document.querySelector('.task-modal');
     const newTaskContainer = document.createElement('div');
-    const createTaskBtn = document.createElement('button');
+    const decisionBtn = document.createElement('button');
     const exitBtn = document.createElement('button');
     const textareaClasses = ['title', 'details'];
 
@@ -105,19 +160,19 @@ export function newTaskDetails() {
         }
     });
 
-    createTaskBtn.innerHTML = 'Create Task';
-    exitBtn.innerHTML = 'X';
+    
+    decisionBtn.setAttribute('id', 'decision');
 
-    createTaskBtn.classList.add('create-task');
+    exitBtn.innerHTML = 'X';
     exitBtn.classList.add('exit-details');
 
     newTaskContainer.classList.add('hidden');
     newTaskContainer.setAttribute('id', 'task-details');
-    newTaskContainer.append(createTaskBtn, exitBtn)
+    newTaskContainer.append(decisionBtn, exitBtn)
     modalSelect.appendChild(newTaskContainer);
 
-    closeTaskDetails(exitBtn, newTaskContainer);
-    createTaskDetails(createTaskBtn);
+    exitBtnEvent(exitBtn, newTaskContainer);
+    decisionBtnEvent(decisionBtn, newTaskContainer);
 }
 
 function additionalTasks(modalForm) {
