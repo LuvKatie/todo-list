0bd1ce8fdcea186a601acdbbeec9f6b1aa9f8e9e
Tony W <bobacrypto2@gmail.com>
2022-12-20 03:17:25 -0800

    Create new pseudo code file | Add pseudo notes for next session | removed todo_tasks js file

---
+++ src/script/pseudo_notes.js
@@ -0,0 +1,16 @@
+// 12 - 20 - 22
+
+// Just finished fixing save functionality
+
+// Move on to being able to edit Projects themselves with another popup modal
+
+// Test adding deleting Projects first
+// Test draw and drop re-arranging for Projects
+
+// Final touches should be a drop-down menu filter for Project priority
+// Nav bar should include a " Completed " section and " In Progress "
+
+// Restyle the page to fit criteria aka have 1 main project showing and an " All Projects " tab on the left
+// In THIs ^^^^ instance we might just create a separate HTML page for the Main landing page and redirect the " All Projects " link back to our current setup
+// Swap index.html to the 1 main project page, and refactor the rest of our stuff so that our existing code DOM appends to a new html file titled "All-Projects"
+// Could possibly do this with the other pages too
